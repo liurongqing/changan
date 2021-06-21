1bf40961aa78a9f5ebba297c8675c6f0660b05a5
liurongqing <phpgege@gmail.com>
2021-06-21 17:49:29 +0800

    1

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.log('222')
+
+function add(a, b) {
+  return a + b
+}
+
+var a = add(1, 2)
+console.log(a)
---
+++ www/main.js
@@ -0,0 +1,1 @@
+(()=>{console.log("222");function l(o,n){return o+n}var a=l(1,2);console.log(a);})();
